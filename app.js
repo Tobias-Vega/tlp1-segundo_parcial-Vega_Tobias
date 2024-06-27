40f3f714dfb268fedd7c6b5c87cd896086d64c8b
Tobias <tobiasvega1210@gmail.com>
2024-06-27 15:42:01 -0300

    Se realizó el metodo PUT

--- app.js
+++ app.js
@@ -32,5 +32,29 @@ app.post('/books', (req,res) => {
     res.json({msg: "Libro creado correctamente"})
 })
 
+app.put('/books/:id', (req,res) => {
+    const id = parseInt(req.params.id)
+    const {title, author, year} = req.body
+
+    const getBook = books.find((book) => book.id === id)
+
+    getBook.title = title
+    getBook.author = author
+    getBook.year = year
+
+    res.json({msg: "datos actualizados"})
+})
+
+app.delete('/books/:id', (req,res) => {
+    const id = parseInt(req.params.id)
+
+    const getBook = books.find((book) => book.id === id)
+
+    const indexBook = books.indexOf(getBook)
+    const deleteBook = books.splice(indexBook, 1);
+
+    res.json({msg: 'Libro eliminado'})
+})
+
 
 app.listen(3000, () => console.log("Servidor corriendo en el puerto 3000"));
